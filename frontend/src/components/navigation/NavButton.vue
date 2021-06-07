b93d0c465130cc3710d7de0aaca579d7b6b5ab56
Kanersps <kanepetra@gmail.com>
2021-06-07 18:32:24 +0200

    tweak: responsive navigation

--- frontend/src/components/navigation/NavButton.vue
+++ frontend/src/components/navigation/NavButton.vue
@@ -1,6 +1,9 @@
 <template>
-  <router-link v-bind:to="link" class="outline-none">
-    <button v-bind:class="{ 'bg-gray-700': active || isPathActive }" class="pl-4 pr-4 pt-2 pb-2 focus:outline-none hover:bg-gray-600 rounded transition-all"> <slot></slot> </button>
+  <router-link v-bind:to="link" class="outline-none block">
+    <button v-bind:class="{ 'bg-gray-700': (active || isPathActive), 'bg-gray-800': !(active || isPathActive) }"
+            class="pl-4 pr-4 pt-2 pb-2 w-full md:w-auto focus:outline-none hover:bg-gray-600 md:rounded transition-all">
+      <slot></slot>
+    </button>
   </router-link>
 </template>
 
@@ -12,8 +15,7 @@ export default {
     active: Boolean
   },
   computed: {
-    isPathActive: function() {
-      console.log()
+    isPathActive: function () {
       return this.$route.path.split("/")[1] === this.link.replace(/\//g, "");
     }
   }
--- frontend/src/components/navigation/NavMenu.vue
+++ frontend/src/components/navigation/NavMenu.vue
@@ -1,26 +1,42 @@
 <template>
-  <nav class="bg-gray-300 dark:bg-gray-800 dark:text-white p-2">
-    <div class="pl-4 w-full pr-4 flex items-center">
-      <router-link class="flex-1" to="/">
-        <span class="text-2xl font-light">Gym<span class="text-yellow-500 font-bold">T</span></span>
-      </router-link>
+  <nav class="bg-gray-300 dark:bg-gray-800 dark:text-white p-2 relative">
+    <div class="pl-1 md:pl-4 w-full pr-4 flex items-center">
+      <div class="flex-1 flex items-center">
+        <svg v-on:click="open = !open" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline md:hidden" fill="none"
+             viewBox="0 0 24 24"
+             stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+        </svg>
+        <router-link to="/" class="text-2xl font-light pl-3 md:pl-0">Gym<span class="text-yellow-500 font-bold">T</span>
+        </router-link>
+      </div>
 
-      <span class="flex gap-2">
+      <div class="flex gap-2 invisible md:visible">
         <NavButton link="/">Home</NavButton>
         <NavButton link="/account">Account</NavButton>
-      </span>
+      </div>
+    </div>
+
+    <div :class="{ 'hidden': !open }" class="md:hidden w-full bg-gray-300 fixed left-0 z-10">
+      <div class="grid mt-2 grid-cols-1 gap-2">
+        <NavButton link="/">Home</NavButton>
+        <NavButton link="/account">Account</NavButton>
+      </div>
     </div>
   </nav>
 </template>
 
 <script>
 import NavButton from "@/components/navigation/NavButton";
+
 export default {
-name: "NavMenu",
-  components: {NavButton}
+  name: "NavMenu",
+  components: {NavButton},
+  data: function () {
+    return {open: false};
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
