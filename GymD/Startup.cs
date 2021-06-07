6ba7e9b903295c0b30473d403cdc7c49bddb4922
Kanersps <kanepetra@gmail.com>
2021-06-07 17:42:16 +0200

    tweak: remove .DS_Store

--- GymD/Startup.cs
+++ GymD/Startup.cs
@@ -23,8 +23,6 @@ namespace GymD
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
-            services.AddTransient<>()
             services.AddSpaStaticFiles(options =>
             {
                 options.RootPath = "frontend/dist";
